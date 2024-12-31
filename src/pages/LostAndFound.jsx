03b846aeca41c1d652ee9fe54c8790ae39720b65
anjumanara28 <anjuanny2024@gmail.com>
2024-12-31 00:34:28 +0000

    added search funtionality on all items page

--- src/pages/LostAndFound.jsx
+++ src/pages/LostAndFound.jsx
@@ -8,17 +8,29 @@ import { Helmet } from "react-helmet";
 const LostAndFound = () => {
   const { loading, setLoading } = useContext(AuthContext);
   const [allItems, setAllItems] = useState([]);
-
-  const fetchAllItems = async () => {
-    setLoading(true);
-    const { data } = await axios.get(`http://localhost:5000/allItems`);
-    setAllItems(data);
-  };
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
+    const fetchAllItems = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get(
+          `http://localhost:5000/allItems?search=${search}`
+        );
+        setAllItems(data);
+      } catch (error) {
+        console.error("Error fetching items:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     fetchAllItems();
-    setLoading(false);
-  }, []);
+  }, [search]);
+
+  const handleReset =()=>{
+    setSearch('')
+  }
 
   return (
     <div>
@@ -31,12 +43,39 @@ const LostAndFound = () => {
         <meta name="keywords" content="React, Helmet, SEO, Example" />
       </Helmet>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14">
+      <div className="flex justify-center gap-4 my-4">
+        {/* search input field */}
+        <div className="flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
+          <input
+            className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
+            type="text"
+            name="search"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+            placeholder="Enter item Title"
+            aria-label="Enter item Title"
+          />
+
+          <button className="btn ">
+            Search
+          </button>
+        </div>
+
+        {/* reset button */}
+        <button
+          onClick={handleReset}
+          className="btn"
+        >
+          Reset
+        </button>
+      </div>
+ 
+  {/* render all items */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-14 mt-3">
         {allItems.map((item) => (
           <div key={item._id} className="card w-96 bg-base-100 shadow-xl">
             <div className="card-body">
               <h2 className="card-title">{item.title}</h2>
-              {/* <p className="text-gray-600">{item.description}</p> */}
               <div className="mt-4">
                 <p>
                   <span className="font-bold">Category: {item.category}</span>
